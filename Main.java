6fbcf907a1caba038eeb58cf70e1435d60861f22
frosya <efrosiniya24@gmail.com>
2023-08-27 14:21:04 +0300

    \n

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Здравствуй, мир!");
+        System.out.println("Здравствуй, \n мир!");
     }
 }
