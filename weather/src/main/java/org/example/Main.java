796aed7153b5696207a43e851781748fb610af91
Efrosiniya24 <123854150+Efrosiniya24@users.noreply.github.com>
2023-09-04 18:44:31 +0300

    Add files via upload

---
+++ weather/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.net.URL;
+import org.w3c.dom.Document;
+
+public class Main {
+    private static Document getPage(){
+String url = "https://yandex.by/pogoda/ru-RU/details/10-day-weather/today?lat=53.902735&lon=27.555691&via=ms";
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
