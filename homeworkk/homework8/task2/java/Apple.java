bdb2ee48a4312f8229141cfde341fb349db9af09
frosya <efrosiniya24@gmail.com>
2023-09-20 22:23:15 +0300

    homework8

--- homeworkk/homework8/task2/java/Apple.java
+++ homeworkk/homework8/task2/java/Apple.java
@@ -1,3 +1,16 @@
-public class Apple {
+import java.util.Scanner;
 
+public class Apple {
+    private String color = "red";
+
+    public Apple(String color) {
+        this.color = color;
+    }
+
+    public Apple() {
+    }
+
+    public String getColor() {
+        return color;
+    }
 }
--- homeworkk/homework8/task2/java/main12.java
+++ homeworkk/homework8/task2/java/main12.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class main12 {
     public static void main(String[] args) {
 //        Создать класс Apple и добавить в него поле color с модификатором доступа private и
 //        инициализировать его. В методе main другого класса создать объект Apple, и не
 //        используя сеттеры изменить значение поля color.
+
+        Scanner sc = new Scanner(System.in);
+
+        Apple apple = new Apple();
+        System.out.println("Значение поля color до изменения: " + apple.getColor());
+
+        System.out.print("Введите цвет яблока: ");
+        String color = sc.nextLine();
+
+        apple = new Apple(color);
+        System.out.println("Значение поля color после изменения: " + apple.getColor());
+
+
     }
 }
