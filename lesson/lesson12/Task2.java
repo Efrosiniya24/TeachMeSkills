ea11e2f53e3e88c88300e9924fe98310cef3d063
frosya <efrosiniya24@gmail.com>
2023-10-05 20:18:18 +0300

    lesson12 task2

--- lesson/lesson12/Task2.java
+++ lesson/lesson12/Task2.java
@@ -1,4 +1,30 @@
 package lesson12;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Task2 {
+    public static void main(String[] args) throws FileNotFoundException {
+//        Задача 2:
+//        Используя FileReader и FileWriter сначала записать в файл tms2.txt фразу на латыни
+//“Cognosce te ipsum”, а затем считать ее и вывести в консоль первую букву.
+
+        Scanner sc = new Scanner(System.in);
+        try(FileWriter fileWriter = new FileWriter("tms2.txt")) {
+            fileWriter.write("Cognosce te ipsum");
+            fileWriter.flush();
+        } catch (IOException e) {
+            System.out.println("Строка записана в файл");
+            throw new RuntimeException(e);
+        }
+
+        try(FileReader fileReader = new FileReader("tms2.txt")){
+            System.out.println((char)fileReader.read() + " -- первый символ строки “Cognosce te ipsum”");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
