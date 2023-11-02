733e839b45bd24d9e28e94bf55cd22ae5d7ab418
frosya <efrosiniya24@gmail.com>
2023-11-02 21:46:55 +0300

    .

---
+++ lesson/lesson18/Hendler.java
@@ -0,0 +1,23 @@
+package lesson18;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class Hendler extends DefaultHandler {
+    boolean name = false;
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException{
+        if(qName.equalsIgnoreCase("line")){
+            name = true;
+        }
+    }
+    @Override
+    public void characters (char ch[], int start, int length) throws SAXException
+    {
+        if(name){
+            System.out.println("Name: " + new String(ch, start, length));
+            name = false;
+        }
+    }
+}
--- lesson/lesson18/SAX.java
+++ lesson/lesson18/SAX.java
@@ -1,7 +1,27 @@
 package lesson18;
 
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+
 public class SAX {
     public static void main(String[] args) {
-
+        final String nameFile = "lesson/lesson18/file.xml";
+        try{
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+            Hendler hendler = new Hendler();
+            saxParser.parse(nameFile, hendler);
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        }  catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        catch (SAXException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
